024bd66d0a116e260933c230cb6e9a8b1330fa35
Emmanuel Martinez <emmanuelmartinez@emmanuels-mbp.att.net>
2016-12-10 15:00:43 -0800

    working on individual inmate info

--- main.py
+++ main.py
@@ -1,17 +1,45 @@
 from bs4 import BeautifulSoup
+from time import sleep
 
 import requests
+import random
 
 
+root_url = 'http://www.tdcj.state.tx.us/death_row/'
 url = 'http://www.tdcj.state.tx.us/death_row/dr_executed_offenders.html'
-response = requests.get(url)
-html = response.text
 
+min_sec = 0.5
+max_sec = 2.7
+
+
+
+
+def url_to_soup(url):
+    response = requests.get(url)
+    html = response.text
+    soup = BeautifulSoup(html, "lxml")
+    
+    return soup
+
+ 
 #things I want from the first page: execution number and link to full bio
-
-soup = BeautifulSoup(html, "lxml")
+soup = url_to_soup(url)
 table = soup.find('table')
 links = table.findAll('a', href=True, text='Offender Information')
 
+inmate_links = []
+
 for link in links:
-    print link['href']
+    inmate_link = root_url + link['href']
+    inmate_links.append(inmate_link)
+    
+
+for inmate in inmate_links[0:1]:
+    sleep(random.uniform(min_sec, max_sec))
+    soup = url_to_soup(inmate)
+    
+    table = soup.find('table', {'class','tabledata_deathrow_table'})
+    print table
+    
+    
+    
